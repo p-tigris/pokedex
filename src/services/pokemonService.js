58c6d3628c56d4216d56a5eeb38d85a79d6c0045
p-tigris <shoumik_dhar@yahoo.com>
2025-11-10 13:26:20 -0500

    added pokemon api service, fetchAllPokemon

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -1,14 +1,15 @@
-const showPokemonData = async (pokemon) => {
+const showPokemonData = async () => {
     try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon" + pokemon);
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0");
 
         if (!res.ok) {
             throw new Error("Failed to fetch data.");
         }
-        
-        const data = res.json();
-        return data;
+
+        return res.json(); 
     } catch (error) {
         console.log(error);
     }
 };
+
+export { showPokemonData }
